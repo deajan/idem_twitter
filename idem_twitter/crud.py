a935b95e7e873aa673ee18d9bf1cb346f5519376
Orsiris de Jong <ozy@netpower.fr>
2022-11-23 13:34:51 +0100

    Add session commit

--- idem_twitter/crud.py
+++ idem_twitter/crud.py
@@ -10,14 +10,16 @@ def tweet_create(tweet: TweetCreate):
     tw = Tweet.from_orm(tweet)
     print(tw)
     tw.save()
+    tw.session.commit()
     return tw
 
-def tweet_get(id: int = None):
-    if id:
-        return Tweet().find(id)
+def tweet_get(id_tweet: int = None):
+    if id_tweet:
+        return Tweet().find(id_tweet)
     return Tweet().all()
 
 
-def tweet_delete(id: int):
-    tweet = Tweet.find(id)
-    tweet.delete()
+def tweet_delete(id_tweet: int):
+    tw = Tweet.find(id_tweet)
+    tw.delete()
+    tw.session.commit()
